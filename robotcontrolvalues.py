0879fdd4c0988ca36965dea9b8883b4277b6b0d2
LOWRYW <lowryw@brpsk12.org>
2016-01-16 14:43:40 -0500

    Game Controller Layout Update
    updated layout

--- robotcontrolvalues.py
+++ robotcontrolvalues.py
@@ -1,13 +1,20 @@
 """
 Game Control Button Values:
-x axis:
-y axis:
+
+x axis = left joypad x axis
+y axis = left joypad y axis
+z axis = right joypad x axis
+z rotation = right joypad y axis
 
 
-button 1
-button 2
-button 3
-button 4
-button 5
-button 6
+button 1 = x
+button 2 = a
+button 3 = b
+button 4 = y
+button 5 = left bumper
+button 6 = right bumper
+button 7 = left trigger
+button 8 = right trigger
+button 9 = select
+button 10 = start
 """
