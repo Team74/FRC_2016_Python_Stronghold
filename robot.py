fa82929709a06a22cfa091d1deee7ea96669c5e3
WubbaDuck <WubbaDuck@users.noreply.github.com>
2016-01-21 13:00:39 -0500

    Added a line for the right stick y axis

--- robot.py
+++ robot.py
@@ -58,7 +58,8 @@ class MyRobot(wpilib.SampleRobot):
             #self.rmotor.set(self.controller1.getRightY())
 
             self.lmotor.set(self.stick.getRawAxis(1))
-            self.rmotor.set(self.stick.getTwist())      # Forks for right stick x axis
+            self.rmotor.set(self.stick.getRawAxis(5))    # might be axis 4
+           # self.rmotor.set(self.stick.getTwist())      # Works for right stick x axis
 
     def test(self):
         wpilib.LiveWindow.run()
