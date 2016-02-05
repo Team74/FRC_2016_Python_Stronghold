ffbba9148c560773647399683087483ea3f3c7ed
LOWRYW <lowryw@brpsk12.org>
2016-02-04 20:24:12 -0500

    This Does NOT WORK
    - The CODE DOESNT WORK IN THIS BRANCH.
    - WE DONT WANT IT

--- components/driveCOPY.py
+++ components/driveCOPY.py
@@ -1,21 +1,32 @@
-@@ -16,13 +16,12 @@ class driveTrain(Component) :
+"""
+File Author: Will Lowry
+File Creation Date: 1/28/2015
+File Purpose: To create our drive functions
+"""
+import wpilib
+from wpilib import CANTalon, Encoder, Timer, RobotDrive
+from wpilib.interfaces import Gyro
+from xbox import XboxController
+from . import Component
+
+class driveTrain(Component) :
+
+    def __init__(self, robot):
+        super().__init__()
         self.robot = robot
 
         # Constants
-        WHEEL_DIAMETER = 6
         WHEEL_DIAMETER = 8
         PI = 3.1415
         ENCODER_TICK_COUNT = 250
         ENCODER_GOAL = 0 # default
-        ENCODER_TOLERANCE = 1 # inch
-        self.CONTROL_TYPE = 0 # 0 = disable PID components and encoder components
-        self.MOTOR_MODE = 1 # 0 = disable the second set of motors
         ENCODER_TOLERANCE = 1 # inch0
         self.CONTROL_TYPE = 1 # 0 = disable PID components
 
         self.rfmotor = CANTalon(0)
         self.rbmotor = CANTalon(1)
-@@ -31,34 +30,58 @@ class driveTrain(Component) :
+        self.lfmotor = CANTalon(2)
+        self.lbmotor = CANTalon(3)
         self.lfmotor.setInverted(True)
         self.lbmotor.setInverted(True)
 
@@ -25,7 +36,6 @@
         self.lbencoder = Encoder(4, 5, False)#, Encoder.EncodingType.k4x)
         self.rbencoder = Encoder(6, 7, False)#, Encoder.EncodingType.k4x)
 
-        self.drive = RobotDrive(self.lmotor, self.rmotor)
         # Set the distance per encoder tick
         self.lfencoder.setDistancePerPulse(WHEEL_DIAMETER*PI/ENCODER_TICK_COUNT)
         self.rfencoder.setDistancePerPulse(WHEEL_DIAMETER*PI/ENCODER_TICK_COUNT)
@@ -38,34 +48,12 @@
         wpilib.LiveWindow.addSensor("Drive Train", "Left Back Encoder", self.lbencoder)
         wpilib.LiveWindow.addSensor("Drive Train", "Right Back Encoder", self.rbencoder)
 
-        if self.CONTROL_TYPE:
-            # Initializing the encoders
-            self.lencoder = Encoder(0, 1, False, Encoder.EncodingType.k4X) #Creates an object of type Encoder, called lencoder. It counts
-            self.rencoder = Encoder(2, 3, True, Encoder.EncodingType.k4X) #the amount that a motor has rotated, and returns it in Direction and Distance variables
 
-            # Set the distance per encoder tick
-            self.lencoder.setDistancePerPulse(WHEEL_DIAMETER*PI/ENCODER_TICK_COUNT)
-            self.rencoder.setDistancePerPulse(WHEEL_DIAMETER*PI/ENCODER_TICK_COUNT)
         self.drive = RobotDrive(self.lfmotor, self.rfmotor, self.lbmotor, self.rbmotor)
 
         if self.CONTROL_TYPE:
 
             # Initializing PID Controls
-            self.pidRight = wpilib.PIDController(0.0, 0.0, 0.0, 0.0, self.rencoder, self.rmotor, 0.02)
-            self.pidLeft = wpilib.PIDController(0.0, 0.0, 0.0, 0.0, self.lencoder, self.lmotor, 0.02)
-
-            # PID Settings
-            self.pidRight.setContinuous(False)
-            self.pidLeft.setContinuous(False)
-            self.pidRight.setAbsoluteTolerance(0.05)
-            self.pidLeft.setAbsoluteTolerance(0.05)
-            self.pidRight.setOutputRange(-1, 1)
-            self.pidLeft.setOutputRange(-1, 1)
-
-            wpilib.LiveWindow.addSensor("Drive Train", "Left Encoder", self.lencoder)
-            wpilib.LiveWindow.addSensor("Drive Train", "Right Encoder", self.rencoder)
-            wpilib.LiveWindow.addActuator("Drive Trian", "Right PID", self.pidRight)
-            wpilib.LiveWindow.addActuator("Drive Trian", "Left PID", self.pidLeft)
             self.pidRightFront = wpilib.PIDController(0.0, 0.0, 0.0, 0.0, self.rfencoder, self.rfmotor, 0.02)
             self.pidLeftFront = wpilib.PIDController(0.0, 0.0, 0.0, 0.0, self.lfencoder, self.lfmotor, 0.02)
             self.pidRightBack = wpilib.PIDController(0.0, 0.0, 0.0, 0.0, self.rbencoder, self.rbmotor, 0.02)
@@ -97,16 +85,20 @@
 
         self.controller = XboxController(0)
 
-@@ -76,12 +99,15 @@ class driveTrain(Component) :
+        self.autonomousSpeed = 0.2
+
+        self.dashTimer = Timer()     # Timer for SmartDashboard updating
+        self.dashTimer.start()
+
+        # Adding components to the LiveWindow (testing)
+        wpilib.LiveWindow.addActuator("Drive Train", "Left Front Motor", self.lfmotor)
+        wpilib.LiveWindow.addActuator("Drive Train", "Right Front Motor", self.rfmotor)
+        wpilib.LiveWindow.addActuator("Drive Train", "Left Back Motor", self.lbmotor)
+        wpilib.LiveWindow.addActuator("Drive Train", "Right Back Motor", self.rbmotor)
+
 
     def log(self):
         '''The log method puts interesting information to the SmartDashboard.'''
-        if self.CONTROL_TYPE:
-            wpilib.SmartDashboard.putNumber("Left Distance", self.lencoder.getDistance())
-            wpilib.SmartDashboard.putNumber("Right Distance", self.rencoder.getDistance())
-            wpilib.SmartDashboard.putNumber("Left Speed", self.lencoder.getRate())
-            wpilib.SmartDashboard.putNumber("Right Speed", self.rencoder.getRate())
-            #wpilib.SmartDashboard.putNumber("Gyro", self.gyro.getAngle())
         wpilib.SmartDashboard.putNumber("Left Front Distance", self.lfencoder.getDistance())
         wpilib.SmartDashboard.putNumber("Right Front Distance", self.rfencoder.getDistance())
         wpilib.SmartDashboard.putNumber("Left Back Distance", self.lbencoder.getDistance())
@@ -119,7 +111,10 @@
 
 # drive forward function
     def drive_forward(self, speed) :
-@@ -92,6 +118,13 @@ class driveTrain(Component) :
+        self.rfmotor.set(speed)
+        self.rbmotor.set(speed)
+        self.lfmotor.set(speed)
+        self.lbmotor.set(speed)
 
 # manual drive function for Tank Drive
     def xboxTankDrive(self, leftSpeed, rightSpeed):
@@ -133,23 +128,39 @@
         self.lfmotor.set(leftSpeed)
         self.rfmotor.set(rightSpeed)
         self.lbmotor.set(leftSpeed)
-@@ -103,8 +136,8 @@ class driveTrain(Component) :
+        self.rbmotor.set(rightSpeed)
+
+# stop function
+    def drive_stop(self) :
+        self.lfmotor.set(0)
         self.rfmotor.set(0)
         self.lbmotor.set(0)
         self.rbmotor.set(0)
-
-'''
 
         '''
 # function to tell us whether or not the goal distance has been reached
     def at_distance_goal(self):
         l_error = self.encoder_goal - self.l_encoder.getDistance()
-@@ -117,7 +150,7 @@ class driveTrain(Component) :
+        r_error = self.encoder_goal - self.r_encoder.getDistance()
+        return abs(l_error) < self.encoder_tolerance and abs(r_error) < self.encoder_tolerance
+
+# function to continue driving if not at goal distance
+    def drive_distance(self) :
+        while not at_drive_goal() :
             self.drive_forward(self.autonomousSpeed)
 
         self.drive_stop()
-'''
         '''
 # fucntion to reset the gyro
     def reset(self):
         if self.CONTROL_TYPE:
+            self.lencoder.reset()
+            self.rencoder.reset()
+
+# function to turn a certain number of degrees
+    def turn_angle(self):
+        print('Turn Angle')
+
+
+    def getDistance(self):
+        return (self.lencoder.getDistance() + self.rencoder.getDistance())/2.0
